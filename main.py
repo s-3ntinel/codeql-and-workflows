78ecfdc91cbffbd9f3b9858829df59310584b82e
pripum <pripum@c002096206.n0cosa.emac.cat.com>
2024-09-11 15:52:45 +0200

    :)

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import os, sys
+import os
+import sys
 
-bearer = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+bearer = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9."
+    "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4"
+    "gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMe"
+    "KKF2QT4fwpMeJf36POk6yJV_adQssw5c"
 os.system(sys.argv[1])
