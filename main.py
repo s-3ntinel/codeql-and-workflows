c6a296b11b807bd72e09e1c342880ff9498a4f60
pripum <pripum@c002096206.n0cosa.emac.cat.com>
2024-09-11 16:02:26 +0200

    :)

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
 import sys
 
-bearer = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9."\
-    "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4"\
-    "gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMe"\
-    "KKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+bearer = (
+    "***"
+    "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4"
+    "gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMe"
+     "KKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+)
 os.system(sys.argv[1])
