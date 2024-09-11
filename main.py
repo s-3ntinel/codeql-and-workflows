fbbe0e4f50cad9f4b4072d81526f041c9cac748a
pripum <pripum@c002096206.n0cosa.emac.cat.com>
2024-09-11 16:05:15 +0200

    :)

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ bearer = (
     "***"
     "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4"
     "gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMe"
-     "KKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+    "KKF2QT4fwpMeJf36POk6yJV_adQssw5c"
 )
 os.system(sys.argv[1])
