23c6cbf5163ad54282aef2c365be537a4a5232d3
pripum <pripum@c002096206.n0cosa.emac.cat.com>
2024-09-23 10:29:56 +0200

    :)

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask import Flask, request, abort
 
 app = Flask(__name__)
 
+bearer = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+
 @app.route("/user_picture1")
 def user_picture1():
     base_path = '/server/static/images'
