12bd7c7dd0eb2d66d428277c9d2e71d7f83cb1fe
test <test@test.test>
2025-03-11 10:01:34 +0100

    :)

--- App.java
+++ App.java
@@ -1,18 +1,12 @@
 public class App {
   public static void main(String args[]) {
-    int random_int = 1;
-    String aristotelian_feaberry = System	.getenv("PREDABLE_PORKET");
-    
-    java.sql.Connection con = java.sql.DriverManager.getConnection(jdbc.toString(),	stonesoup_mysql_user, stonesoup_mysql_pass);
-    java.sql.Statement stmt = con.createStatement();
-    
-    String queryString = "INSERT INTO Shippers (ShipperID, CompanyName)"
-    											+ " VALUES (\'"
-    											+ random_int
-    											+ "\', \'"
-    											+ aristotelian_feaberry
-    											+ "\');";
-    stmt.execute(queryString);
-    con.close();
+	String host = System.getenv("PREDABLE_PORKET");
+
+	Process proc;
+	try {
+		proc = Runtime.getRuntime().exec(new String[] { "bash", "-c", "ping -c1 " + host });
+	} catch(Exception e) {
+	}
+
   }
 }
