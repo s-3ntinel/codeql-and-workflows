3b58d3cccfbdafcf26b0d22ffdb331162a7aae4e
test <test@test.test>
2025-03-19 09:18:34 +0100

    :)

--- App.java
+++ App.java
@@ -1,36 +1,7 @@
-public class App {
-        public static String killDriver(DriverType driverType) {                                                                                      
-                String driverPath = null;                                                                                                           
-                switch (driverType) {                                                                                                                                                                                                                                                                    
-                case FIREFOX:                                                                                                                       
-                        driverPath = DataManager.getFirefoxDriverPath();                                                                            
-                        break;                                                                                                                      
-                case CHROME:                                                                                                                        
-                        driverPath = DataManager.getChromeDriverPath();                                                                             
-                        break;                                                                                                                      
-                case ELECTRON:                                                                                                                      
-                        driverPath = DataManager.getElectronDriverPath();                                                                           
-                        break;                                                                                                                      
-                case IE:                                                                                                                            
-                        driverPath = DataManager.getIeDriverPath();
-                        break;
-                case EDGE:
-                        driverPath = DataManager.getEdgeDriverPath();
-                        break;
-                case HEADLESS:
-                        driverPath = DataManager.getChromeDriverPath();
-                        break;
-                default:
-                        log.Error("No driver found for the given driver type:" + driverType);
-                        break;
-                }
-                String[] driverPathSplit = driverPath.split("/");
-                String driverExe = driverPathSplit[driverPathSplit.length - 1];
-                String command = "taskkill /F /IM " + driverExe;
-
-try {
-                        Runtime.getRuntime().exec(command);
-                } catch (IOException e) {
-                }
-        }
+class App {
+    public static void main(String[] args) {
+        // BAD: user input might include special characters such as ampersands
+		M m = new M();
+		m.met(args[1]);
+}
 }
