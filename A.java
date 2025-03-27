ebc35d90710000802a6954db81472fa173921892
test <test@test.test>
2025-03-27 14:27:40 +0100

    :)

--- A.java
+++ A.java
@@ -3,12 +3,12 @@ import java.io.File;
 import javax.servlet.http.HttpServletRequest;
 
 public class A {
-	public void HandleRequest(HttpServletRequest request) {
+	public void HandleRequest(String request) {
         //String filename = System.getenv("input");
-		String filename = request.getHeader("X-File-Name");
+		//String filename = request.getHeader("X-File-Name");
 
 		try {
-	        FileOutputStream fos = new FileOutputStream(new File(filename));
+	        FileOutputStream fos = new FileOutputStream(new File(request));
 		} catch(Exception e) {}
     }
 }
