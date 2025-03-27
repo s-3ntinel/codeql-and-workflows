f73a0dfeee4dc32263a5855889478a40d1c1481e
test <test@test.test>
2025-03-27 14:18:25 +0100

    :)

--- A.java
+++ A.java
@@ -1,9 +1,11 @@
 import java.io.FileOutputStream;
 import java.io.File;
+import javax.servlet.http.HttpServletRequest;
 
 public class A {
-	public static void main(String args[]) {
-        String filename = System.getenv("input");
+	public void HandleRequest(HttpServletRequest request) {
+        //String filename = System.getenv("input");
+		String filename = request.getHeader("X-File-Name");
 
 		try {
 	        FileOutputStream fos = new FileOutputStream(new File(filename));
