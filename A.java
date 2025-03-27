03c1e1721995c36782d3398231914b19801a528e
test <test@test.test>
2025-03-27 14:47:56 +0100

    :)

--- A.java
+++ A.java
@@ -3,11 +3,11 @@ import java.io.File;
 import java.util.Scanner;
 
 public class A {
-	public void HandleRequest(String in) {
-		//String filename = request.getHeader("X-File-Name");
+	public void HandleRequest(HttpServletRequest request) {
+		String filename = request.getHeader("X-File-Name");
 
 		try {
-	        FileOutputStream fos = new FileOutputStream(new File(in));
+	        FileOutputStream fos = new FileOutputStream(new File(filename));
 		} catch(Exception e) {}
     }
 }
