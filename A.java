a7462d29156d7223ce0c8d2f45a86f982407ad32
test <test@test.test>
2025-03-27 14:44:50 +0100

    :)

--- A.java
+++ A.java
@@ -3,12 +3,9 @@ import java.io.File;
 import java.util.Scanner;
 
 public class A {
-	public void HandleRequest() {
-        //String filename = System.getenv("input");
+	public void HandleRequest(String in) {
 		//String filename = request.getHeader("X-File-Name");
 
-		Scanner scanner = new Scanner(System.in);
-		String in = scanner.nextLine();
 		try {
 	        FileOutputStream fos = new FileOutputStream(new File(in));
 		} catch(Exception e) {}
