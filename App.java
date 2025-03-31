9f14dad4ffce7e307fe78eb53b85c388869ef7e6
test <test@test.test>
2025-03-31 14:06:59 +0200

    :)

---
+++ App.java
@@ -0,0 +1,13 @@
+public class App {
+	public static void main(String args[]) {
+		java.sql.Connection con = java.sql.DriverManager.getConnection(
+			jdbc.toString(),
+			mysql_user,
+			mysql_password
+		);
+
+		java.sql.Statement stmt = con.createStatement();
+		String query = "SELECT * FROM users WHERE username = '" + input_username + "' AND password = '" + input_password + "'";
+		stmt.execute(query);
+	}
+}
