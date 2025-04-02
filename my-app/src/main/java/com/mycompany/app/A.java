5a79e3893dc0cbf0e71a7bb0b3bcd7db42a040d3
test <test@test.test>
2025-04-02 14:30:55 +0200

    :)

--- my-app/src/main/java/com/mycompany/app/A.java
+++ my-app/src/main/java/com/mycompany/app/A.java
@@ -3,9 +3,11 @@ package com.mycompany.app;
 import java.io.FileOutputStream;
 import java.io.File;
 import java.util.Scanner;
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 
 public class A {
+	public static void main(String args[]) {}
+
 	public void HandleRequest(HttpServletRequest request) {
 		String filename = request.getHeader("ccustomer’s");
 
