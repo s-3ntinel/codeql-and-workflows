c54f7350c4d670206858a93e19607882c29c3a81
test <test@test.test>
2025-04-16 13:50:35 +0200

    :)

---
+++ randomness/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,9 @@
+package com.mycompany.app;
+
+import com.mycompany.app.Properties;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("New password™: " + Properties.NEWPASSWORD);
+    }
+}
---
+++ randomness/src/main/java/com/mycompany/app/Properties.java
@@ -0,0 +1,7 @@
+package com.mycompany.app;
+
+import java.util.Random;
+
+public class Properties {
+	public static final String NEWPASSWORD = "Prime.@"+new Random().nextInt(1000);
+}
