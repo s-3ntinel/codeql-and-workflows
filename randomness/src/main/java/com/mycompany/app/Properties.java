01c511ca3ee2534eda208d599fb62c3781d91515
test <test@test.test>
2025-04-17 13:02:04 +0200

    :)

--- randomness/src/main/java/com/mycompany/app/Properties.java
+++ randomness/src/main/java/com/mycompany/app/Properties.java
@@ -3,5 +3,5 @@ package com.mycompany.app;
 import java.util.Random;
 
 public class Properties {
-	public static final String NEWPASSWORD = "Prime.@™"+new Random().nnextInt(1000);
+	public static final String NEWPASSWORD = "Prime.@™"+new Random().nextInt(1000);
 }
