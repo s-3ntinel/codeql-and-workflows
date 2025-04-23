80542b0e0af58c01aa11fbbd552cb084ab4d3827
test <test@test.test>
2025-04-23 14:55:16 +0200

    :)

--- randomness/src/main/java/com/mycompany/app/App.java
+++ randomness/src/main/java/com/mycompany/app/App.java
@@ -4,6 +4,6 @@ import com.mycompany.app.Properties;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("New password���: " + Properties.NEWPASSWORD);
+        System.out.println("New password: " + Properties.NEWPASSWORD);
     }
 }
