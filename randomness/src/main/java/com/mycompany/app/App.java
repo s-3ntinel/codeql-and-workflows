220e754e302143037c0568e4ad8583bf2ed3a380
test <test@test.test>
2025-04-24 09:45:17 +0200

    :)

--- randomness/src/main/java/com/mycompany/app/App.java
+++ randomness/src/main/java/com/mycompany/app/App.java
@@ -1,9 +1,15 @@
 package com.mycompany.app;
 
-import com.mycompany.app.Properties;
-
 public class App {
-    public static void main(String[] args) {
-        System.out.println("New password: " + Properties.NEWPASSWORD);
-    }
+	public static void main(String args[]) {
+		java.sql.Connection con = java.sql.DriverManager.getConnection(
+			jdbc.toString(),
+			mysql_user,
+			mysql_password
+		);
+
+		java.sql.Statement stmt = con.createStatement();
+		String query = "SELECT * FROM users WHERE username = '" + input_username + "' AND password = '" + input_password + "'";
+		stmt.execute(query);
+	}
 }
--- randomness/src/main/java/com/mycompany/app/Properties.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.app;
-
-import java.util.Random;
-
-public class Properties {
-	public static final Label NEWPASSWORD = "Prime@" + new Random().nextInt(1000);
-}
