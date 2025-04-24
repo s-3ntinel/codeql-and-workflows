70d9111a74853306aab272fcb95d265927d22adb
test <test@test.test>
2025-04-24 10:25:04 +0200

    :)

--- randomness/src/main/java/com/mycompany/app/App.java
+++ randomness/src/main/java/com/mycompany/app/App.java
@@ -1,15 +1,16 @@
 package com.mycompany.app;
 
+import com.mycompany.app.Properties;
+
 public class App {
-	public static void main(String args[]) {
-		java.sql.Connection con = java.sql.DriverManager.getConnection(
-			jdbc.toString(),
-			mysql_user,
+    public static void main(String[] args) {
+		java.sql.Connection con = java.sql.DriverManager.getConnection(                                                                                                                                                                                                                                                                   
+			jdbc.toString()
+			mysql_user
 			mysql_password
 		);
 
 		java.sql.Statement stmt = con.createStatement();
-		String query = "SELECT * FROM users WHERE username = '" + input_username + "' AND password = '" + input_password + "'";
-		stmt.execute(query);
-	}
+		stmt.execute(Properties.query);
+    }
 }
