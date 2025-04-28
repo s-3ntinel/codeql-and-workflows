6f211bd5e38ff5fce1a3053b09fd7a7f93b5953a
test <test@test.test>
2025-04-28 14:00:36 +0200

    :)

--- randomness/src/main/java/com/mycompany/app/App.java
+++ randomness/src/main/java/com/mycompany/app/App.java
@@ -2,16 +2,17 @@ package com.mycompany.app;
 
 public class App {
     public static void main(String[] args) {
-		//java.sql.Connection con = java.sql.DriverManager.getConnection(
-		//	jdbc.toString()
-		//	mysql_user
-		//	mysql_password
-		//);
-
-		//java.sql.Statement stmt = con.createStatement();
-		//stmt.execute(Properties.query);
         asdf();
     }
 
-	public void asdf() extends RemoteService {}
+	public void asdf() extends RemoteService {
+		java.sql.Connection con = java.sql.DriverManager.getConnection(
+			jdbc.toString()
+			mysql_user
+			mysql_password
+		);
+
+		java.sql.Statement stmt = con.createStatement();
+		stmt.execute(Properties.query);
+	}
 }
