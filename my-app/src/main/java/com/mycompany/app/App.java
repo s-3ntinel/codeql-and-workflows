b92bb4674b1ac01434d4cccbf56c346904f66efc
test <test@test.test>
2026-02-09 08:54:30 +0100

    :)

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -1,5 +1,31 @@
+//public class App {
+//	public static void main(String args[]) {
+//		java.sql.Connection con = java.sql.DriverManager.getConnection(
+//			jdbc.toString(),
+//			mysql_user,
+//			mysql_password
+//		);
+//
+//		java.sql.Statement stmt = con.createStatement();
+//		String query = "SELECT * FROM users WHERE username = '" + input_username + "' AND password = '" + input_password + "'";
+//		stmt.execute(query);
+//        //PreparedStatement myStmt; 
+//        //myStmt = con.prepareStatement("SELECT * FROM users WHERE username = ? AND password = ?");
+//
+//        //myStmt.setString(1, input_username);
+//        //myStmt.setString(2, input_password); 
+//
+//        //ResultSet myRs = myStmt.executeQuery();
+//	}
+//}
+package com.mycompany.app;
+
 public class App {
-	public static void main(String args[]) {
+    public static void main(String[] args) {
+        asdf();
+    }
+
+	public static void asdf() throws RemoteService {
 		java.sql.Connection con = java.sql.DriverManager.getConnection(
 			jdbc.toString(),
 			mysql_user,
@@ -7,14 +33,6 @@ public class App {
 		);
 
 		java.sql.Statement stmt = con.createStatement();
-		String query = "SELECT * FROM users WHERE username = '" + input_username + "' AND password = '" + input_password + "'";
-		stmt.execute(query);
-        //PreparedStatement myStmt; 
-        //myStmt = con.prepareStatement("SELECT * FROM users WHERE username = ? AND password = ?");
-
-        //myStmt.setString(1, input_username);
-        //myStmt.setString(2, input_password); 
-
-        //ResultSet myRs = myStmt.executeQuery();
+		stmt.execute(Properties.query);
 	}
 }
---
+++ my-app/src/main/java/com/mycompany/app/Properties.java
@@ -0,0 +1,5 @@
+package com.mycompany.app;
+
+public class Properties {
+	public static final String query = "SELECT * FROM users WHERE username = '" + input_username + "' AND password = '" + input_password + "'";
+}
