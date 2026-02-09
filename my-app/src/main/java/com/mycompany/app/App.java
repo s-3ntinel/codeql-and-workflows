28f599ecd1b95a3e591fc88878e6cfe406c8a913
test <test@test.test>
2026-02-09 09:05:49 +0100

    :)

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -22,10 +22,6 @@ package com.mycompany.app;
 
 public class App {
     public static void main(String[] args) {
-        asdf();
-    }
-
-	public static void asdf() throws RemoteService {
 		java.sql.Connection con = java.sql.DriverManager.getConnection(
 			jdbc.toString(),
 			mysql_user,
@@ -33,6 +29,15 @@ public class App {
 		);
 
 		java.sql.Statement stmt = con.createStatement();
-		stmt.execute(Properties.query);
+        public static final String query = "SELECT * FROM users WHERE username = '" + input_username + "' AND password = '" + input_password + "'";
+		stmt.execute(query);
+
+        //PreparedStatement myStmt; 
+        //myStmt = con.prepareStatement("SELECT * FROM users WHERE username = ? AND password = ?");
+
+        //myStmt.setString(1, input_username);
+        //myStmt.setString(2, input_password); 
+
+        //ResultSet myRs = myStmt.executeQuery();
 	}
 }
