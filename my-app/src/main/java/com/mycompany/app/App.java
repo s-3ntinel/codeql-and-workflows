14bf111bb9891162bb69ea990d05c1b529f3ff13
test <test@test.test>
2026-02-09 09:17:30 +0100

    :)

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -11,16 +11,16 @@ public class App {
         String input_username = "username";
         String input_password = "password";
 
-		//java.sql.Statement stmt = con.createStatement();
-        //String query = "SELECT * FROM users WHERE username = '" + input_username + "' AND password = '" + input_password + "'";
-		//stmt.execute(query);
+		java.sql.Statement stmt = con.createStatement();
+        String query = "SELECT * FROM users WHERE username = '" + input_username + "' AND password = '" + input_password + "'";
+		stmt.execute(query);
 
-        PreparedStatement myStmt; 
-        myStmt = con.prepareStatement("SELECT * FROM users WHERE username = ? AND password = ?");
+        //PreparedStatement myStmt; 
+        //myStmt = con.prepareStatement("SELECT * FROM users WHERE username = ? AND password = ?");
 
-        myStmt.setString(1, input_username);
-        myStmt.setString(2, input_password); 
+        //myStmt.setString(1, input_username);
+        //myStmt.setString(2, input_password); 
 
-        ResultSet myRs = myStmt.executeQuery();
+        //ResultSet myRs = myStmt.executeQuery();
 	}
 }
