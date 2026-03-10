7360b082330625f11b29f7c0ffbc462166eab8f9
test <test@test.test>
2026-03-10 14:42:36 +0100

    :)

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -2,22 +2,25 @@ package com.mycompany.app;
 
 public class App {
     public static void main(String[] args) {
+		mysql_user = "admin";
+		mysql_password = "SuperSecretPassword123"
+
 		java.sql.Connection con = java.sql.DriverManager.getConnection(
 			jdbc.toString(),
 			mysql_user,
 			mysql_password
 		);
 
-		//java.sql.Statement stmt = con.createStatement();
-        //String query = "SELECT * FROM users WHERE username = '" + input_username + "' AND password = '" + input_password + "'";
-		//stmt.execute(query);
+		java.sql.Statement stmt = con.createStatement();
+        String query = "SELECT * FROM users WHERE username = '" + input_username + "' AND password = '" + input_password + "'";
+		stmt.execute(query);
 
-        PreparedStatement myStmt; 
-        myStmt = con.prepareStatement("SELECT * FROM users WHERE username = ? AND password = ?");
+        //PreparedStatement myStmt; 
+        //myStmt = con.prepareStatement("SELECT * FROM users WHERE username = ? AND password = ?");
 
-        myStmt.setString(1, input_username);
-        myStmt.setString(2, input_password); 
+        //myStmt.setString(1, input_username);
+        //myStmt.setString(2, input_password); 
 
-        ResultSet myRs = myStmt.executeQuery();
+        //ResultSet myRs = myStmt.executeQuery();
 	}
 }
